fc5d3e07649ca430329c0e144a8338a6901bd6cf
Alexandre Pinto <alexcp@gmail.com>
2013-02-04 19:21:19 -0200

    HW1: Problem 4 finished

--- Assign01/Problem4.R
+++ Assign01/Problem4.R
@@ -31,3 +31,13 @@ bps <- c(0, 2000, 3000, 5000, 10000, 50000)
 hist(smokers$GDPPerCapita, breaks=bps, main="Histogram of GDP Per Capita", xlab="GDP Per Capita", col.main = "purple")
 
 ## i)
+
+GDP <- smokers$GDPPerCapita
+labels = c("0 to 2000", "2000 to 3000", "3000 to 5000", "5000 to 10000", "10000 to 50000")
+X <- c(length(GDP[GDP < 2000]),
+       length(GDP[GDP >= 2000 & GDP < 3000 ]),
+       length(GDP[GDP >= 3000 & GDP < 5000 ]),
+       length(GDP[GDP >= 5000 & GDP < 10000 ]),
+       length(GDP[GDP >= 10000 & GDP < 50000 ]))
+X
+pie(X, labels, clockwise=TRUE, main="Pie Chart of GDP Per Capita", col.main = "purple")
